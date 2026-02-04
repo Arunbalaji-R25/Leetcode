829a4364814e998de7364899ac802816d0fdaf82
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-02-04 20:44:23 +0530

    Time: 22 ms (99.46%) | Memory: 53.3 MB (99.76%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,41 +1,65 @@
+// import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>>lst=new ArrayList<>();
-        HashSet<List<Integer>>set=new HashSet<>();
-    
-        
-        Arrays.sort(nums);
+        List<List<Integer>> results = new ArrayList<>();
+        sort(nums);
 
-        for(int i=0;i<nums.length-2;i++){
-            
-            
-            int left=i+1,right=nums.length-1;
-            while(left<right){
-                List<Integer>subset=new ArrayList<>();
-                if(nums[i]+nums[left]+nums[right]==0){
-                    subset.add(nums[i]);
-                    subset.add(nums[left]);
-                    subset.add(nums[right]);
-                    set.add(subset);
-                    
-                    left++;
-                    right--;
+        int n = nums.length;
+        for (int i = 0; i < n; i++){
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            if (nums[i] > 0) break;
 
-                }
-                if(nums[i]+nums[left]+nums[right]>0){
-                    right--;
-                }
-                if(nums[i]+nums[left]+nums[right]<0){
-                    left++;
-                }
+            int j = i+1;
+            int k = n-1;
 
-
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                } else if (sum > 0){
+                    k--;
+                } else {
+                    results.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                    while (j < k && nums[j] == nums[j-1]) j++;                }
             }
-              
         }
-        for(List<Integer> values:set){
-            lst.add(values);
+        return results;
+    }
+    int[] aux;
+    private void sort(int[] arr) {
+        aux = new int[arr.length];
+        sort(arr, 0, arr.length);
+    }
+
+    private void sort(int[] arr, int low, int high) {
+        if (low+1 < high) {
+            int mid = low + (high-low)/2;
+
+            sort(arr, low, mid);
+            sort(arr, mid, high);
+
+            merge(arr, low, high);
         }
-        return lst;
+    }
+
+    private void merge(int[] arr, int low, int high) {
+        int l = low, mid = low + (high-low)/2, m = mid, n = high-low;
+
+        for (int i = 0; i < n; i++) {
+            if (l == mid) {
+                aux[i] = arr[m++];
+            } else if (m == high) {
+                aux[i] = arr[l++];
+            } else if (arr[l] <= arr[m]) {
+                aux[i] = arr[l++];
+            } else {
+                aux[i] = arr[m++];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[low+i] = aux[i];
+        }
     }
 }
