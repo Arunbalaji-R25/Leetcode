e4f9a0baece340aedb5c6f650e9df2fdd132ca59
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-02-06 09:21:23 +0530

    Time: 2 ms (60.97%) | Memory: 46.6 MB (51.52%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
-        char[] r = ransomNote.toCharArray();
-        char[] m = magazine.toCharArray();
+        int[] cnt = new int[26];
 
-        Arrays.sort(r);
-        Arrays.sort(m);
-
-        int i=0,j=0;
-
-        while(i<r.length && j<m.length){
-            if(r[i] == m[j]){
-                i++;
-                j++;
-            }else if(r[i]>m[j]){
-                j++;
-            }else{
-                return false;
-            }
+        for(char c : magazine.toCharArray()) {
+            cnt[c-'a']++;
         }
-        return i == r.length;
+
+        for(char c : ransomNote.toCharArray()) {
+            cnt[c-'a']--;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(cnt[i] < 0) return false;
+        }
+
+        return true;
     }
 }
