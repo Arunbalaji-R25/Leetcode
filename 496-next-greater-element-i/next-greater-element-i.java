4d23140b2435af947d288ba411449704affe7178
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-03-04 09:13:30 +0530

    Time: 3 ms (92.95%) | Memory: 45.1 MB (73.83%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map = new HashMap<>();
+        Deque<Integer> st = new ArrayDeque<>();
+        for(int x:nums2){
+            while(!st.isEmpty() && st.peek()<x){
+                map.put(st.pop(),x);
+            }
+            st.push(x);
+        }
+     
+        int ans[] = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = map.getOrDefault(nums1[i],-1);
+        }
+        return ans;  
+    } 
+}
