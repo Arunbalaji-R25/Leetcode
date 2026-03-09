df1fb5e5dd2592d28712ab853ba829dc3e19ee7a
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-03-09 09:35:36 +0530

    Time: 15 ms (68.69%) | Memory: 53.1 MB (5.11%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,45 +1,22 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if(matrix==null || matrix.length==0){
-            return 0;
-        }
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] mat = new int[m][n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                mat[i][j] = matrix[i][j] - '0';
-            }
-        }
-        
-        for(int i=0;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(mat[i][j]==1){
-                    mat[i][j] += mat[i][j-1];
-                }
-            }
-        }
-
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] height = new int[m];
+        Arrays.fill(height, 0);
         int ans = 0;
-
-        for(int j=0;j<n;j++){
-            for(int i=0;i<m;i++){
-                int width = mat[i][j];
-                if(width==0) continue;
-
-                int currwidth = width;
-                for(int k=i;k<m && mat[k][j]>0;k++){
-                    currwidth = Math.min(currwidth,mat[k][j]);
-                    int height = k-i+1;
-                    ans = Math.max(ans,currwidth*height);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (matrix[i][j] == '0') {
+                    height[j] = 0;
+                } else {
+                    height[j]++;
                 }
-
-                currwidth = width;
-                for(int k=i;k>=0 && mat[k][j]>0;k--){
-                    currwidth = Math.min(currwidth,mat[k][j]);
-                    int height = i-k+1;
-                    ans = Math.max(ans,currwidth*height);
+                int h = height[j];
+                for (int k = j; k >=0; --k) {
+                    h = Math.min(h, height[k]);
+                    if (h == 0) break;
+                    ans = Math.max(ans, h * (j - k + 1));
                 }
             }
         }
