9a45085896eb3d55f560101ca65d003dfde5803a
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-03-30 21:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (66.71%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        long ans = 1;
+        while(n>4){
+            ans *= 3;
+            n -= 3;
+        }
+        ans *= n;
+        return (int) ans;
+    }
+}
