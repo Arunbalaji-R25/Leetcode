2e515d6e0a5aa96a854ca3e7ff75347f1f7ee1b6
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-04-09 10:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (21.79%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -4,10 +4,17 @@
  *     int val;
  *     ListNode next;
  *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val) { 
+     this.val = val;
+     }
+ *     ListNode(int val, ListNode next) { 
+     this.val = val;
+     this.next = next; 
+    }
  * }
  */
+
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode curr = head;
