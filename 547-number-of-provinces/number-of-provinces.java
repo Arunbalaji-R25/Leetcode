f63c8714a40575f2f34778ba37c4454b3653a722
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-07-06 10:27:22 +0530

    Time: 1 ms (87.10%) | Memory: 46.9 MB (97.40%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,27 +1,22 @@
 class Solution {
-    public void bfs(int[][] isConnected,boolean[] visited,int s){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(s);
-        visited[s] = true;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            for(int i=0;i<isConnected.length;i++){
-                if(!visited[i] && isConnected[node][i] == 1){
-                    q.offer(i);
-                    visited[i] = true;
-                }
-            }
-        }
-    }
     public int findCircleNum(int[][] isConnected) {
-        boolean[] visited = new boolean[isConnected.length];
-        int count = 0;
-        for(int i=0;i<isConnected.length;i++){
-            if(!visited[i]){
-                bfs(isConnected,visited,i);
-                count++;
+        int n=isConnected.length;
+        int comp=0;
+        boolean vis[]=new boolean [n];
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,isConnected,vis);
+                comp++;
             }
         }
-        return count;
+        return comp;
+    }
+    public void dfs(int v,int[][] isConnected,boolean vis[]){
+        vis[v]=true;
+        for(int i=0;i<isConnected.length;i++){
+            if(isConnected[v][i]==1 && !vis[i]){
+                dfs(i,isConnected,vis);
+            }
+        }
     }
 }
