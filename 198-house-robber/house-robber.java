e7bbf5ad76d85fd5d68d8822042104856a30d5fd
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2026-07-08 10:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (37.16%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev = 0;
+        int max = 0;
+        for(int i:nums){
+            int temp = Math.max(max,prev+i);
+            prev = max;
+            max = temp;
+        }
+        return max;
+    }
+}
